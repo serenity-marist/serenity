c50bf75ce9127ca15a7a5a2da591c1b8f5fbdf97
Ariel Camilo <arielcamilo97@gmail.com>
2018-09-21 16:01:29 -0400

    autoLogin enabled

--- DegreeWorksStudentView.py
+++ DegreeWorksStudentView.py
@@ -21,12 +21,36 @@ from flask import Flask
 app = Flask(__name__)
 
 
-driver = webdriver.Chrome(executable_path='/Users/alexaj/anaconda3/bin/chromedriver')
-    #Url to DWORKS
+usernameStr =
+passwordStr =
+
+
+
+driver = webdriver.Chrome(executable_path='/Users/arielcamilo/downloads/chromedriver')
 url = "https://degreeworks.banner.marist.edu/dashboard/dashboard"
 driver.get(url)
 
+
+
+username = driver.find_element_by_id('username')
+username.send_keys(usernameStr)
+
+password = driver.find_element_by_id('password')
+password.send_keys(passwordStr)
+
+nextButton = driver.find_element_by_css_selector('#welcome > div > div.row.btn-row > input.btn-submit')
+nextButton.click()
+
+
+
+# driver = webdriver.Chrome(executable_path='/Users/arielcamilo/downloads/chromedriver')
+#     #Url to DWORKS
+# url = "https://degreeworks.banner.marist.edu/dashboard/dashboard"
+# driver.get(url)
+# delay = 10
 #driver.implicity_wait(10)
+
+print("LOL")
 try:
     element = WebDriverWait(driver, 20).until(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'html > frameset > frame:nth-child(4)'))
--- api_start.py
+++ api_start.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 api = Api(app)
 
 
+
+
+@app.route('/')
+def index():
+    import DegreeWorksStudentView
+    return 'Hello World'
+
+
+
+
+
 def index(req):
     postData = req.form
-    json = str(postData['param'].value) 
+    json = str(postData['param'].value)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
