1d097b5b191ce43e945c31f8d8941207255e5165
Alexa Javellana <AlexaJ@148-100-183-209.FoxNet.marist.edu>
2018-10-09 23:28:31 -0400

    ui

--- routes.py
+++ routes.py
@@ -20,6 +20,10 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/loading')
+def loading():
+    return render_template('loading.html')
+
 @app.route('/test', methods =['GET','POST'])
 def test():
     return "data"
