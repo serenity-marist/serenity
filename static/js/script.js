578a70fc75190f05017dd4205d9d5d3f0706168b
Gary Coltrane <coltranegary@gmail.com>
2018-10-11 01:10:13 -0400

    made changes to script

--- static/js/script.js
+++ static/js/script.js
@@ -8,14 +8,18 @@ $(function() {
             data: submissionData,
             type: 'POST',
             success: function(body){
-              var test = `{"Student": "Coltrane, Gary", "ID": "20066526", "Classification": "Senior", "Advisors": "Coleman, Ronald G Rice, Mary C", "Overall GPA": "3.359", "Student Type": "Traditional Continuing", "Term Location": "HEOP", "Level": "Undergraduate", "Degree": "B.S.", "College": "Computer Science & Mathematics", "Major": "Computer Science", "Minor": "CompSci: Software Development"}`
               var parse = JSON.parse(body);
-              console.log(parse);
               var {College, Major, Level, Student,ID,Classification} = parse
                 $(".main-content").empty();
                 $(".main-content").append("<h1> Success </h1>")
                 $(".main-content").append(`<p> ${Student}: ${ID}</p>`)
                 $(".main-content").append(`${College} ${Major}: ${Level}`)
+
+
+
+            },
+            error: function(body){
+              $(".main-content").prepend(`<h1> There was an error logging you in! Please try again</h1>`);
 
 
 
