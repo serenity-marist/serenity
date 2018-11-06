775d11d000286779e93cbfb0b1498827d5f2ac28
Alexa Javellana <AlexaJ@148-100-133-213.FoxNet.marist.edu>
2018-11-06 12:47:25 -0500

    changes

--- routes.py
+++ routes.py
@@ -59,5 +59,5 @@ def login():
    return jsonify(settings.email)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
